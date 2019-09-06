6e915bd4d53d4c064e6f45bd7c2343968c33762e
yuanrenwei <1172016748@qq.com>
2019-09-06 09:07:20 +0800

    Initial commit

---
+++ src/T.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class T {
+    public static void main(String[] args){
+
+        ArrayList arrayList=new ArrayList();
+        System.out.println(arrayList.size());
+    }
+}
